2fdc038a4a8f6bf3e941bb3dc38a822442b388c5
K16E, Inc <kb@k16e.co>
2025-02-08 23:17:38 +0100

    ...

--- src/scripts/motion/motion-cover.js
+++ src/scripts/motion/motion-cover.js
@@ -4,9 +4,21 @@ import { animate, stagger } from 'motion'
 export function _motionCover() {
     const section = _q('[data-motion]')
     const ts = _ql('h1 span', section)
+    const symbol = _q('[data-motion="symbol"]', section)
+    const link = _q('[data-motion="link"]', section)
+    const logo = _q('[data-motion="logo"]', section)
 
     animate(ts,
-        { y: [50,0], opacity: 1 },
+        { y: [50,0], opacity: [0,1] },
         { delay: stagger(0.1), duration: 1, ease: 'circInOut' }
+    )
+
+    animate(symbol,
+        { x: [-100,0], opacity: [0,1] },
+        { delay: 1, duration: 0.35, ease: 'circInOut' }
+    )
+    animate(link,
+        { y: [100, 0], opacity: [0, 1] },
+        { delay: 1.5, duration: 0.75, ease: 'circInOut' }
     )
 }
