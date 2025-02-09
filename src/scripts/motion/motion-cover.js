27e4256e2a9b43cc416685ed2a64a2841b6dee73
K16E, Inc <kb@k16e.co>
2025-02-09 07:10:28 +0100

    timelined sequenced motion

--- src/scripts/motion/motion-cover.js
+++ src/scripts/motion/motion-cover.js
@@ -2,32 +2,38 @@ import { _q, _ql } from '@scripts/utils/snips'
 import { animate, stagger } from 'motion'
 
 export function _motionCover() {
-    const section = _q('[data-motion]')
-    const ts = _ql('h1 span', section)
-    const symbol = _q('[data-motion="symbol"]', section)
-    const link = _q('[data-motion="link"]', section)
-    const logo = _q('[data-motion="logo"]', section)
-    const caption = _q('[data-motion="caption"]', section)
+    const wrapper = _q('[data-motion-wrapper]')
+    const ts = _ql('[data-motion="heading"]', wrapper)
+    const symbol = _q('[data-motion="symbol"]', wrapper)
+    const link = _q('[data-motion="link"]', wrapper)
+    const logo = _q('[data-motion="logo"]', wrapper)
+    const caption = _q('[data-motion="caption"]', wrapper)
 
-    animate(ts,
-        { y: [50,0], opacity: [0,1] },
-        { delay: stagger(0.1), duration: 1, ease: 'circInOut' }
-    )
-
-    animate(symbol,
-        { x: [-100,0], opacity: [0,1] },
-        { delay: 0.5, duration: 0.35, ease: 'circInOut' }
-    )
-    animate(link,
-        { y: [100, 0], opacity: [0, 1] },
-        { delay: 0.75, duration: 0.75, ease: 'circInOut' }
-    )
-    animate(logo,
-        { y: [100, 0], opacity: [0, 1] },
-        { delay: 1, duration: 1, ease: 'circInOut' }
-    )
-    animate(caption,
-        { x: [100, 0], opacity: [0, 1] },
-        { delay: 1.25, duration: 1, ease: 'circInOut' }
-    )
+    animate([
+        [
+            ts,
+            { y: [48, 0], opacity: [0, 1] },
+            { delay: stagger(0.1), duration: 0.5, ease: 'circInOut' }
+        ],
+        [
+            symbol,
+            { x: [-100, 0], opacity: [0, 1] },
+            { duration: 0.3, ease: 'circInOut' }
+        ],
+        [
+            link,
+            { y: [100, 0], opacity: [0, 1] },
+            { duration: 0.5, ease: 'circInOut' }
+        ],
+        [
+            logo,
+            { y: [100, 0], opacity: [0, 1] },
+            { duration: 0.5, ease: 'circInOut' }
+        ],
+        [
+            caption,
+            { x: [100, 0], opacity: [0, 1] },
+            { duration: 0.5, ease: 'circInOut' }
+        ]
+    ])
 }
