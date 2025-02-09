108b26c7c7725d4f7afd188f7230b1fc9331fbf5
K16E, Inc <kb@k16e.co>
2025-02-09 23:14:07 +0100

    ...

--- src/scripts/motion/motion-links.js
+++ src/scripts/motion/motion-links.js
@@ -2,7 +2,7 @@ import { _q, _ql } from '@scripts/utils/snips'
 import { animate, hover } from 'motion'
 
 export function _motionLinks() {
-    const spring = _ql('[data-motion="link-hover"]')
+    const spring = _ql('[data-link="hover"]')
 
     hover(spring, (i) => {
         animate(i, { scale: 1.15 }, { type: 'spring', duration: 0.35 })
