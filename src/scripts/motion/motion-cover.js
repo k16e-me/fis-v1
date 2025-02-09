b8d14080ca1948fbcc94e51d40e6208e94032403
K16E, Inc <kb@k16e.co>
2025-02-09 16:55:30 +0100

    poc ellipsis animation

--- src/scripts/motion/motion-cover.js
+++ src/scripts/motion/motion-cover.js
@@ -8,12 +8,18 @@ export function _motionCover() {
     const link = _q('[data-motion="link"]', wrapper)
     const logo = _q('[data-motion="logo"]', wrapper)
     const caption = _q('[data-motion="caption"]', wrapper)
+    const ellipsis = _ql('[data-motion="ellipsis"]', wrapper)
 
     animate([
         [
             ts,
             { y: [48, 0], opacity: [0, 1] },
             { delay: stagger(0.1), duration: 0.4, ease: 'circInOut' }
+        ],
+        [
+            ellipsis,
+            { y: [4, 0], opacity: [0, 1] },
+            { delay: stagger(0.1), duration: 0.4, ease: 'linear' }
         ],
         [
             link,
